710fd74b34a7320bc01145137d38449557383ade
GabrielMello777 <gabrielmello8986@gmail.com>
2025-05-09 12:33:47 -0300

    adicionei a logo

--- src/app/tabs/_layout.tsx
+++ src/app/tabs/_layout.tsx
@@ -1,9 +1,25 @@
 import { Tabs } from "expo-router";
+import { Image, Text, View } from 'react-native';
 
 export default function TabsLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="home" options={{ title: "Compras" }} />
+      <Tabs.Screen 
+        name="home" 
+        options={{ 
+          title: "Compras", 
+          headerTitle: () => (
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Image 
+                source={require("../../../assets/icon.png")} 
+                style={{ width: 120, height: 120, marginRight: 8 }} 
+                resizeMode="contain"
+              />
+              <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Compras</Text>
+            </View>
+          )
+        }}  
+      />
       <Tabs.Screen name="teste" options={{ title: "Teste" }} />
     </Tabs>
   );
